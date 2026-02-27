a1ac7115cddc524b34c59071117599d47bce5882
gideb <gideblarbi@gmail.com>
2026-02-27 05:06:52 +0000

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import AppRoutes from './routes/AppRoutes.jsx'
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/next";
 
 const App = () => {
   return (
     <>
       <AppRoutes />
       <SpeedInsights />
+      <Analytics />
     </>
   );
 };
